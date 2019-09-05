4eed068196085d4633f26d24db7930926d3504da
Edward997 <edward.navarrete7@gmail.com>
2019-09-05 10:38:56 -0500

    Suscripcion revistas

--- Practica 2.cpp
+++ Practica 2.cpp
@@ -2,41 +2,55 @@
 #include <conio.h>
 
 using namespace std;
-
-struct direccion{
-	char call[20];
-	int tel;
-	char ciudad[20];
-};
-
-struct empleado{
-	char nombre[20];
-	char genero[20];
-	int edad;
-	struct direccion dir;
+/// sus = Nombre del suscriptor
+///tiempo = tiempo de la suscripcion
+///rev = Nombre de la revista
+struct revista{
+	char sus[20];
+	int tiempo;
+	char rev[20];
 };
 
 int main(int argc, char** argv) {
-	struct empleado E1[5];
-	struct empleado *apuntador;
+	struct revista S1[5];
+	struct revista *apuntador;
+	int op;
+	apuntador=&S1[0];
 	
-	apuntador=&E1[0];
 	
-	for(int i=0; i<5; i++){
-		
-		cout<<"nombre: ";
-		cin.getline(E1[i].nombre,20);
-		cout<<"edad: ";
-		cin>>E1[i].edad;
-		cout<<"calle: ";
-		fflush(stdin);
-		cin.getline(E1[i].dir.call,20);
+	cout<<endl<<"Seleccione una opcion: " <<endl;
+    cout<<"1.- Nuevo suscriptor " << endl;
+    cout<<"2.- Checar suscriptores" << endl;
+    cout<<"3.- Editar suscriptor" << endl;
+    cout<< "-->  ";
+    cin>>  op;
+    
+    switch (op){
+    	case 1: cout<<" Nuevo suscriptor" <<endl;
+    			for(int i=0; i<10; i++){
+					cout<<"Nombre Suscriptor: ";
+					fflush(stdin);
+					cin.getline(S1[i].sus,20);
+					cout<<"Tiempo de las suscripcion (meses): ";
+					cin>>S1[i].tiempo;
+					fflush(stdin);
+					cout<<"Nombre revista: ";
+					cin.getline(S1[i].rev,20);
+					
+				}
+    		break;
+    	
+    	case 2: cout<<" Checar suscriptores";
+    		break;
+    		
+    	case 3: cout<<" Editar suscriptor ";
+    		break;
+    	
+    	default : cout<<" ";
 	}
-	
-	cout<<endl<<"nombre guardado: " <<apuntador->nombre;
-	cout<<endl<<"edad guardada: " <<apuntador->edad;
-	cout<<endl<<"genero guardado: " <<apuntador->genero;
-	cout<<endl<<"calle guardada: " <<apuntador->dir.call;
+	cout<<endl<<"Nombre suscriptor: " <<apuntador->sus;
+	cout<<endl<<"Tipo de revista: " <<apuntador->tiempo;
+	cout<<endl<<"Nombre de la revista: " <<apuntador->rev;
 	
 	return 0;
 }
